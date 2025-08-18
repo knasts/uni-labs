c06f395543585a7b6e3e511af59d1e1105d00e8b
Anastasiia Kulyk <kuliknatasha17@gmail.com>
2025-08-18 20:57:05 +0300

    Add files via upload

---
+++ closest_to_integer.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <math.h>
+
+float calculation(float x, int k)
+{
+    int two = 2 * k;
+    int three = 3 * k; //розраховуємо значення елемента за формулою
+    float number = exp(cos(pow(x, two))) * sin(pow(x,three));
+    return number;
+}
+
+int main()
+{
+    int n, k;
+    float x, num, number;
+    float less_num = 0;
+    float less_abs = 0.6;
+    float current_abs;
+    const int ten = 10;
+
+    printf("Enter the value of n (can`t be less than 1):\n");
+    scanf("%d", &n);
+    printf("Enter the value of x: \n");
+    scanf("%f", &x);
+
+    if(n < 1)
+    {
+        printf("Eror: the value of n can`t be less than 1\n");
+        while(n < 1)
+        {
+            printf("Try again: \n");
+            printf("Enter the value of n (can`t be less than 1):\n");
+            scanf("%d", &n);
+        }
+    }
+
+    for(k = 1; k <= n; k++)
+    {
+        number = calculation(x, k);
+        num = roundf(number);
+        current_abs = fabs(num - number);
+        if(current_abs < less_abs)
+        {
+            less_num = number;
+            less_abs = current_abs;
+        }
+    }
+
+    printf("Our number closest to an integer: %.4f \n" , less_num);
+    return 0;
+}
