71838001ac6f956412853a4310379cdd87c5e0d4
Anastasiia Kulyk <kuliknatasha17@gmail.com>
2025-08-26 18:28:52 +0300

    Update students_struct.c

--- students_struct.c
+++ students_struct.c
@@ -1,8 +1,3 @@
-/*14. Заданий масив структур, який містить прізвища і назви груп.
-Масив впорядкувати за прізвищами. Програма повинна видаляти запис, який
-містить задане прізвище, зсуваючи елементи вперед (заповнюючи ними
-порожнє місце).*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,10 +14,10 @@ void bubble_sort(struct Students* array, int N)
 {
     struct Students temp;
     for(int i = 0; i < N; i++)
-    {                       //-1 щоб не вийти за межі масиву
+    {                       
         for(int j = 0; j < N - i - 1; j++)
         {
-            if(strcmp(array[j].surname, array[j + 1].surname) > 0) //якщо strcmp повертає додатне значення, то перший рядок йде за алфавітом раніше
+            if(strcmp(array[j].surname, array[j + 1].surname) > 0) 
             {
                 temp = array[j];
                 array[j] = array[j + 1];
@@ -39,38 +34,38 @@ void print_list(struct Students* array, int N)
         printf("Surname of the %d student is %s, group %d \n", i + 1 , array[i].surname, array[i].group);
     }
 }
-                  //подвійний вказівник, бо ми знімнюємо не лише вміст, а й сам вказівник
+                  
 int checking(struct Students** array, int* N, struct Students stud)
 {
     int index = -1;
     int i;
     for(i = 0; i < *N; i++)
     {
-        if(strcmp((*array)[i].surname, stud.surname) == 0) //якщо strcmp повертає 0, то перший рядок = другому
+        if(strcmp((*array)[i].surname, stud.surname) == 0) 
         {
             index = i;
             break;
         }
     }
 
-    if(index == -1) //якщо елемент не знайдемо, то повертаємо -1
+    if(index == -1) 
     {
         return -1;
     }
 
     for(i = index; i < *N - 1; i++)
     {
-       (*array)[i] = (*array)[i + 1]; //зміщуємо всі елементи нижче знайденого на 1 вгору
+       (*array)[i] = (*array)[i + 1]; 
     }
 
     (*N)--;
-    struct Students* temp_arr = realloc(*array, (*N) * sizeof(struct Students)); //перевиділяємо пам'ять
+    struct Students* temp_arr = realloc(*array, (*N) * sizeof(struct Students)); 
     if (temp_arr == NULL && *N > 0)
     {
         return -2;
     }
 
-    *array = temp_arr; //якщо пам'ять перевиділилася, то змінюємо наш масив
+    *array = temp_arr; 
     return 0;
 }
 
@@ -95,23 +90,23 @@ int main()
         for(i = 0; i < N; i++)
         {
             printf("Enter surname of the %d student:  ", i + 1);
-            scanf("%19s", arrayy[i].surname); //введення прізвища
+            scanf("%19s", arrayy[i].surname); 
         }
     }
 
     srand(time(0));
     for(i = 0; i < N; i++)
     {
-        arrayy[i].group = (rand() % 20) + 1; //випадково генеруємо номер групи
+        arrayy[i].group = (rand() % 20) + 1; 
     }
 
-    bubble_sort(arrayy, N); // відсортовуємо "бульбашкою" масив за алфавітом
+    bubble_sort(arrayy, N); 
     printf("Our list of the students:\n");
     print_list(arrayy, N);
 
     printf("Enter surname of the student that you are looking for:  ");
     scanf("%19s", student.surname);
-                            //передаємо адреси змінних у функцію
+                           
     int result = checking(&arrayy, &N, student);
     if(result == -1)
     {
@@ -131,3 +126,4 @@ int main()
     free(arrayy);
     return 0;
 }
+
