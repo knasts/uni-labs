3d6744bbacd1f93dc50665565ba24f021873934b
Anastasiia Kulyk <kuliknatasha17@gmail.com>
2025-11-17 21:54:00 +0200

    Add files via upload

---
+++ IsThisBinarySearchTree.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+bool inOrderTrav(Node* node, int& prevData)
+{
+    if (node == nullptr) return true;
+    if (!inOrderTrav(node->left, prevData)) return false;
+
+    if (node->data <= prevData) return false;
+    prevData = node->data;
+
+    return(inOrderTrav(node->right, prevData));
+
+}
+
+bool checkBST(Node* root)
+{
+    int prevData = -1;
+    return inOrderTrav(root, prevData);
+}
+
+int main()
+{
+    
+}
+
